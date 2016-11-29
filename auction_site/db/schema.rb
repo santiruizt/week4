55b51c4f5fb6944f544762ec1bc81012dbdfc27a
santiruizt <sruiztoribio@gmail.com>
2016-11-29 19:22:55 +0100

    Add home auction site

--- auction_site/db/schema.rb
+++ auction_site/db/schema.rb
@@ -10,17 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116154143) do
+ActiveRecord::Schema.define(version: 20161127222645) do
 
   create_table "bids", force: :cascade do |t|
-    t.integer "amount"
-    t.integer "user_id"
-    t.integer "product_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["product_id"], name: "index_bids_on_product_id"
-    t.index ["user_id"], name: "index_bids_on_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
